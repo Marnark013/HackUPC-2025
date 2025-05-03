6f85ad0bc0151634c44a69a9555c9d4281b208d3
anoguermu <137610059+anoguermu@users.noreply.github.com>
2025-05-03 19:28:11 +0200

    done power manager

---
+++ Assets/scripts/CpuManager.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+using System.Linq;
+using System.Collections.Generic;
+public class CpuManager : MonoBehaviour
+{
+    public static CpuManager Instance { get; private set; }
+    private List<CPUTile> Cpus; 
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Cpus = new List<CPUTile>();
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void addCpu(CPUTile tile)
+    {
+        Cpus.Add(tile);
+        Cpus = Cpus.OrderBy(t => t.getFreeTrowhtput()).ToList();
+    }
+
+    public bool addToCpu(int neededTrougthput)
+    {
+        int left = 0;
+        int right = Cpus.Count - 1;
+
+        while (left <= right)
+        {
+            int mid = (left + right) / 2;
+            double midPower = Cpus[mid].getFreeTrowhtput();
+
+            if (midPower == neededTrougthput)
+            {
+                //compute 
+                return true;
+            }
+            else if (midPower < neededTrougthput)
+                left = mid + 1;
+            else
+                right = mid - 1;
+        }
+
+        return false; // Not found
+    }
+
+}
+   
--- Assets/scripts/tiles/CPUTile.cs
+++ Assets/scripts/tiles/CPUTile.cs
@@ -1,6 +1,7 @@
 public class CPUTile : Tile
 {
     public double Eficiency;
+    private double TrowhtputUsed;
     private double PercapacityUsed;
     private double cpuVariableEnergyConsume;
     public double SpaceNeeded;
@@ -12,6 +13,14 @@ public class CPUTile : Tile
 
     }
 
+    public double getTrowhtputUsed()
+    {
+        return TrowhtputUsed;
+    }
+    public double getFreeTrowhtput()
+    {
+        return trowhtput - TrowhtputUsed;
+    }
     public override double EnergyConcumption()
     {
         double energy = PercapacityUsed * cpuVariableEnergyConsume;
