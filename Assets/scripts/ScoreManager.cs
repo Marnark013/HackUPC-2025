aae0b5a8c173908317ba495ac1c55736797de25c
anoguermu <137610059+anoguermu@users.noreply.github.com>
2025-05-03 12:58:03 +0200

    ja podem calcular el maxim comput power

--- Assets/scripts/ScoreManager.cs
+++ Assets/scripts/ScoreManager.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.UI;
+using System;
 public class ScoreManager : MonoBehaviour
 {
     public static ScoreManager Instance;
@@ -9,7 +10,33 @@ public class ScoreManager : MonoBehaviour
     [SerializeField] private Text scoreText;
     [SerializeField] private int dataNeeded;
 
-   
+    public double ComputationPower() {
+        double computpower = 0;
+        double storage = 0;
+        BoardManager board = BoardManager.Instance;
+        Tile[,] tiles = board.getTiles();
+
+        int width = tiles.GetLength(0);  // Number of columns (x)
+        int height = tiles.GetLength(1);
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                Tile tile = tiles[x, y];
+
+                if (tile is DiskTile diskTile)
+                {
+                    storage += diskTile.getMaxData();
+                }else if( tile is CPUTile cpuTile)
+                {
+                    computpower += cpuTile.getTrowhtput();
+                }
+            }
+        }
+        return Math.Min(computpower,storage);
+
+
+    }
 
 
     private void Awake()
--- Assets/scripts/tiles/CPUTile.cs
+++ Assets/scripts/tiles/CPUTile.cs
@@ -2,11 +2,16 @@ public class CPUTile : Tile
 {
     public double Eficiency;
     public double SpaceNeeded;
+    private double trowhtput;
     public int units;
     public CpuUsage Usage;
     public override void Tick()
     {
 
+    }
+    public double getTrowhtput()
+    {
+        return trowhtput;
     }
 }
 public enum CpuUsage
